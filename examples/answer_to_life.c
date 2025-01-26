de94980309f4eb8c9ddd0ea4ad7a6a0b7fd89e7a
Grégoire <puma-cattle-bunch@duck.com>
2025-01-26 18:54:42 +0100

    changed example's name, upgraded the example

--- examples/answer_to_life.c
+++ examples/answer_to_life.c
@@ -7,18 +7,18 @@ result_t fn_no_return(int param)
     if(param != 1)
         err("Quite unpeculiar error, innit?")
     
-    ok(28, int)
+    ok(42, int)
 }
 
 result_t fn(int param)
 {
     result_t res = fn_no_return(param);
 
-    unwrap(res);
+    int answer_to_life = unwrap(res, int);
 
     printf("%s\n", "Does this gets printed?");
 
-    ok(42);
+    ok(answer_to_life);
 }
 
 int main(int argc, char const *argv[])
@@ -27,6 +27,9 @@ int main(int argc, char const *argv[])
     
     int res = unwrap_or_else(a, int) {
         printf("%s - oops\n", a.data);
+
+        // Do not forget to set res!!
+        res = -1;
     }
 
     printf("Answer : %d\n", res);
